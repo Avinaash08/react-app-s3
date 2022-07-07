4d579cd0397942f7ff38bd7f3109ec5d7aea983a
Avinaash08 <38736401+Avinaash08@users.noreply.github.com>
2022-07-07 13:57:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         
-       <Widget name="Hi Indium-POC-Results-CX - RCX-50-a"/>
+       <Widget name="Hi Indium-POC-Results-CX - RCX-50-avin"/>
       </header>
     </div>
   );
