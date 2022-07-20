4ce0d5dbfae9b4d34d3350fb247265791ab48701
Avinaash08 <38736401+Avinaash08@users.noreply.github.com>
2022-07-21 04:34:27 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         
-       <Widget name="Hi Indium-POC-Results-CX test"/>
+       <Widget name="Hi Indium-POC-Results-CX test data"/>
       </header>
     </div>
   );
