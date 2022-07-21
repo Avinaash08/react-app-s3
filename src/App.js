28a00a995e065b41e72fff39b79d6435266a3799
Avinaash08 <38736401+Avinaash08@users.noreply.github.com>
2022-07-21 16:54:26 +0530

    test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         
-       <Widget name="Hi Indium-POC-Results-CX test data testing data error new test data error test1"/>
+       <Widget name="Hi Indium-POC-Results-CX test data testing data error new test data error test1 test2"/>
       </header>
     </div>
   );
