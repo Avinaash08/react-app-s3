db172047c7a074cb0a9f58a8f23e2d042ae83532
Avinaash08 <38736401+Avinaash08@users.noreply.github.com>
2022-07-21 17:04:06 +0530

    commit1

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         
-       <Widget name="Hi Indium-POC-Results-CX test data testing data error new test data error test1 test2"/>
+       <Widget name="Hi Indium-POC-Results-CX commit1"/>
       </header>
     </div>
   );
