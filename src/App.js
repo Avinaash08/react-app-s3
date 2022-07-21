553f588d396a4eeb64d5b5c620cb3ffb941996b1
Avinaash08 <38736401+Avinaash08@users.noreply.github.com>
2022-07-21 19:17:27 +0530

    8

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         
-       <Widget name="Hi Indium-POC-Results-CX commit1 commit2 commit3 commit4 5 6 7"/>
+       <Widget name="Hi Indium-POC-Results-CX commit1 commit2 commit3 commit4 5 6 7 8"/>
       </header>
     </div>
   );
